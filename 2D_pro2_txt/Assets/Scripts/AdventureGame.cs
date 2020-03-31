0a37a49f53bbe71c982403a57d239db03ae1ff83
Jerard Carney <jercarne@uat.edu>
2020-03-30 18:27:30 -0600

    Scriptable Objects
    States Added

---
+++ 2D_pro2_txt/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AdventureGame : MonoBehaviour
+{
+    // public variables initialized (Using serialized)
+    [SerializeField] Text textComponent;
+    [SerializeField] State initialState;
+   
+
+    // Instance to state class
+    State currentState;
+
+    // Private Variables Initialized
+    private string hardcodeTestText = "Coded in C# text.";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = initialState;
+        textComponent.text = currentState.GetStateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D_pro2_txt/Assets/Scripts/States/State.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Allow creation of scriptable object through unity menu
+[CreateAssetMenu(menuName = "State")]
+
+// State machine class... Used for text___________________________________________________________________
+public class State : ScriptableObject   //Scriptable object is a DATA container
+{
+    // creat text box (inspector) 14 lines by 10
+    // serialized make this public in inspector
+    // string to hold text from inspector (CAN HARD CODE THIS ALSO)
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    // serialized make this public in inspector
+    // created arrry to allow option of next state based on current state.
+    [SerializeField] State[] nextStateArray;
+
+    
+    // Public Methods_____________________________________________________________________________________
+
+    // Gets current states text
+    public string GetStateText()
+    {
+        return storyText;
+    }
+
+    // Provides the next state in array form... Varies per current state...
+    public State[] GetNextState()
+    {
+        return nextStateArray;
+    }
+}
+ 
